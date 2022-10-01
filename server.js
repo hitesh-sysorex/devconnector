1b5ed7627fc6c53242776e862e1570180fd2c22d
hitesh-sysorex <hiteshsysorex@gmail.com>
2022-10-01 12:24:27 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const connectDB = require('./config/db');
+connectDB();
+const app = express();
+
+const port = process.env.PORT || 5000;
+// const www = process.env.WWW || './';
+// app.use(express.static(www));
+// console.log(`serving ${www}`);
+
+app.get('/', (req, res) => {
+    res.send(`API Running`);
+});
+app.listen(port, () => console.log(`Server started on port ${port}`));
