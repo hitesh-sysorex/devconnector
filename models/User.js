ed62eee8e1c37a52b7362816c765fd8f1d589f6a
hitesh-sysorex <hiteshsysorex@gmail.com>
2022-10-03 00:50:26 +0530

    User Registration

--- models/User.js
+++ models/User.js
@@ -1,32 +1,29 @@
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
 
-    name: {
-        type: String,
-        required: true,
-    },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
+  password: {
+    type: String,
+    required: true,
+  },
 
-    password: {
-        type: String,
-        required: true
-    },
+  avatar: {
+    type: String,
+  },
 
-    avatar: {
-        type: String,
-
-    },
-
-    date: {
-        type: Date,
-        default: Date.now
-    }
-
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = User = mongoose.mode('User', UserSchema);
+module.exports = User = mongoose.model('User', UserSchema);
